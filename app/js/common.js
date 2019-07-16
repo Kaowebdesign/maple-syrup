b20ecdd4bccc81ea7fb75968f78292c81e342a85
kaowebdesign <kaowebdesign>
2019-07-16 18:01:29 +0300

    upd

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,11 @@
 $( document ).ready(function() {
-
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        dots:false,
+        nav:true,
+        loop:true,
+        navContainerClass:'barrel__dotsWrap',
+        navClass:['barrel__navItem barrel__navItem_prev','barrel__navItem barrel__navItem_next'],
+        navText:['<i class="fa fa-arrow-left barrel__arrow"></i>','<i class="fa fa-arrow-right barrel__arrow"></i>']
+    });
 });
