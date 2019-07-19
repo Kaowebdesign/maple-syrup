32bfbaa148cf0979e62a1775fad7edb7f67e07f4
KaoWebDesign <assdwqe@gmail.com>
2019-07-19 09:10:08 +0300

    upd

--- app/js/common.js
+++ app/js/common.js
@@ -1,25 +1,36 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     // vars
     let prodsBtn = $('#prodsCard .prodsCard__arrow');
     // barrel carousel
     $("#barrelCarousel").owlCarousel({
-        items:1,
-        dots:false,
-        nav:true,
-        loop:true,
-        navContainerClass:'barrel__dotsWrap',
-        navClass:['barrel__navItem barrel__navItem_prev','barrel__navItem barrel__navItem_next'],
-        navText:['<i class="fa fa-arrow-left barrel__arrow"></i>','<i class="fa fa-arrow-right barrel__arrow"></i>']
+        items: 1,
+        dots: false,
+        nav: true,
+        loop: true,
+        navContainerClass: 'barrel__dotsWrap',
+        navClass: ['barrel__navItem barrel__navItem_prev', 'barrel__navItem barrel__navItem_next'],
+        navText: ['<i class="fa fa-arrow-left barrel__arrow"></i>', '<i class="fa fa-arrow-right barrel__arrow"></i>']
     });
     $("#partnerCarousel").owlCarousel({
         items: 3,
-        nav:false,
-        loop:true,
-        dotsClass:'partners__dots mt-5',
-        dotClass:'partners__dot'
+        nav: false,
+        loop: true,
+        dotsClass: 'partners__dots mt-5',
+        dotClass: 'partners__dot',
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1140: {
+                items: 3
+            }
+        }
     });
     // button for prods cards
-    prodsBtn.on('click',function(e){
+    prodsBtn.on('click', function(e) {
         e.preventDefault();
         let thisParent = $(this).parent().parent();
         thisParent.find('.prodsCard__backDuo').toggleClass('prodsCard__backDuo_active');
